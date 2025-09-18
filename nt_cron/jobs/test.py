880cf2a305c9a9615d3158df286aa8c199822d62
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-09-18 15:40:16 -0600

    Fix.

--- nt_cron/jobs/test.py
+++ nt_cron/jobs/test.py
@@ -1,7 +1,6 @@
 import datetime as dt
 from zoneinfo import ZoneInfo
 from nt_cron.slack import schedule_message, Channel, list_scheduled_messages
-from rich import print
 
 def test_run():
     today = dt.date.today()
