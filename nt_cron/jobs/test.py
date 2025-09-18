390c124edadb57175fc17918d26d141864da8ebd
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-09-18 15:51:59 -0600

    Testing.

--- nt_cron/jobs/test.py
+++ nt_cron/jobs/test.py
@@ -1,11 +1,11 @@
 import datetime as dt
 from zoneinfo import ZoneInfo
-from nt_cron.slack import schedule_message, Channel, list_scheduled_messages
+from nt_cron.slack import schedule_message, Channel, list_scheduled_messages, clear_scheduled_messages
 
 def test_run():
     today = dt.date.today()
-    time = dt.time(15, 59, tzinfo=ZoneInfo('America/Denver'))
-    notification_time = dt.datetime.combine(today, time)
+    time = dt.time(16, 0, tzinfo=ZoneInfo('America/Denver'))
+    notification_time = dt.datetime.combine(today, time).astimezone(ZoneInfo('UTC'))
     print(f"notification_time = {notification_time}")
 
     schedule_message(
@@ -13,3 +13,13 @@ def test_run():
         text=f'Test: notification_time = {notification_time}',
         schedule_time=notification_time
     )
+
+# if __name__ == '__main__':
+#     test_run()
+#     print(
+#         list_scheduled_messages(Channel.Testing)
+#     )
+
+    # 1758232740
+
+    # 1758232740
